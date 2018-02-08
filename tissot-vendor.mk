7769b5833fa4e265c0488876467b71b97038cc43
deadman96385 <seanhoyt963@gmail.com>
2018-02-08 11:55:08 -0600

    Revert "TEMP: Move prebuilt files to their own folder"
    This reverts commit c7a7d064c735f5ab77addc2e8c8d74b27e7c3507.

--- tissot-vendor.mk
+++ tissot-vendor.mk
@@ -1,24 +1,2 @@
 # SYSTEM FILES
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/tissot/proprietary,system)
-
-PRODUCT_PACKAGES += \
-    libsdm-disp-apis \
-    libtime_genoff \
-    libts_detected_face_hal \
-    libts_face_beautify_hal \
-    QtiTelephonyService \
-    TimeService \
-    datastatusnotification \
-    embms \
-    fastdormancy \
-    shutdownlistener \
-    CNEService \
-    QtiTetherService \
-    com.qualcomm.location \
-    qcrilmsgtunnel \
-    ims \
-    imssettings \
-    qcrilhook \
-    com.qti.location.sdk \
-    qdcm_calib_data_nt35532_fhd_video_mode_dsi_panel \
-    qdcm_calib_data_nt35596_tianma_fhd_video_mode_dsi_panel
